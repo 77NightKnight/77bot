177943ab0b0e063813952973d962713efe9ae90a
77NightKnight <59813350+77NightKnight@users.noreply.github.com>
2020-01-13 15:36:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -76,6 +76,7 @@ bot.on ('message', msg=>{
             .addField('!kick [member] (optional reason)', 'Kicks a member from the server')
             .setColor('0x30E5BB')
             .setThumbnail('https://i.imgur.com/JsgxK3Y.png')
+            msg.author.send(embed);
             new RichEmbed()
             .setTitle('Music Plugin Commands(Beta)')
             .addField('!play [song]', 'Plays the linked song in the voice channel!')
